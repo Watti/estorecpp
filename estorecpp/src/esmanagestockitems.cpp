8409341ba15c3bf4c0acf4263a25ebfa8dad75c5
imoremada <imoremada@gmail.com>
2016-01-09 16:48:18 +0530

    search function has been re-factored

--- estorecpp/src/esmanagestockitems.cpp
+++ estorecpp/src/esmanagestockitems.cpp
@@ -104,12 +104,10 @@ void ESManageStockItems::slotSearch(QString text)
 		ui.tableWidget->removeRow(0);
 	}
 
-	QString q = "SELECT * FROM item WHERE deleted = 0";
-
 	if (ui.inStockCheckBox->isChecked())
 	{
-		q = "SELECT * FROM item";
-		//q.append(" WHERE ");
+		QString q = "SELECT * FROM item";
+		
 	}
 
 	if (!ui.categoryComboBox->currentText().isEmpty())
@@ -119,82 +117,59 @@ void ESManageStockItems::slotSearch(QString text)
 
 	if (!text.isEmpty())
 	{
-		q = "SELECT * FROM item";
+		QString q = "SELECT * FROM item";
 		q.append(" WHERE deleted = 0 AND ( item_code LIKE '%" + text + "%' OR item_name LIKE '%" + text + "%')");
-	}
 
-	QSqlQuery query1(q);
-	while (query1.next())
-	{
-		QString itemId = query1.value(0).toString();
-		QString itemCategoryId = query1.value(1).toString();
-
-		int row = ui.tableWidget->rowCount();
-		ui.tableWidget->insertRow(row);
-		ui.tableWidget->setItem(row, 0, new QTableWidgetItem(query1.value(2).toString()));
-		ui.tableWidget->setItem(row, 1, new QTableWidgetItem(query1.value(3).toString()));
-
-		QSqlQuery query3("SELECT * FROM item_category WHERE itemcategory_id=" + itemCategoryId);
-		if (query3.first())
+		QSqlQuery query1(q);
+		while (query1.next())
 		{
-			ui.tableWidget->setItem(row, 2, new QTableWidgetItem(query3.value(2).toString()));
-		}
+			QStringList rowItems;
+			QString itemId = query1.value(0).toString();
+			QString itemCategoryId = query1.value(1).toString();
 
-		QString qty = "Not in stock";
-		QString minQty = "Not in stock";
-		QString unitPrice = "Not in stock";
+			rowItems.append(query1.value(2).toString());
+			rowItems.append(query1.value(3).toString());
 
-		QSqlQuery query2("SELECT * FROM stock WHERE item_id=" + itemId);
-		if (query2.first())
-		{
-			qty = query2.value(3).toString();
-			minQty = query2.value(4).toString();
-
-			QSqlQuery query4("SELECT * from item_price WHERE itemprice_id=" + query2.value(2).toString());
-			if (query4.first())
+			QSqlQuery query3("SELECT * FROM item_category WHERE itemcategory_id=" + itemCategoryId);
+			if (query3.first())
 			{
-				unitPrice = query4.value(2).toString();
+				rowItems.append(query3.value(2).toString());
 			}
+
+			QString qty = "Not in stock";
+			QString minQty = "Not in stock";
+			QString unitPrice = "Not in stock";
+
+			QSqlQuery query2("SELECT * FROM stock WHERE item_id=" + itemId);
+			if (query2.first())
+			{
+				qty = query2.value(3).toString();
+				minQty = query2.value(4).toString();
+
+				QSqlQuery query4("SELECT * from item_price WHERE itemprice_id=" + query2.value(2).toString());
+				if (query4.first())
+				{
+					unitPrice = query4.value(2).toString();
+				}
+			}
+
+			rowItems.append(qty);
+			rowItems.append(minQty);
+			rowItems.append(query1.value(5).toString());
+			rowItems.append(unitPrice);
+			rowItems.append(query1.value(6).toString());
+
+			bool inStock = true;
+			if (qty == "Not in stock")
+			{
+				inStock = false;
+			}
+			displayStockTableRow(rowItems, itemId, inStock);
 		}
-
-		ui.tableWidget->setItem(row, 3, new QTableWidgetItem(qty));
-		ui.tableWidget->setItem(row, 4, new QTableWidgetItem(minQty));
-		ui.tableWidget->setItem(row, 5, new QTableWidgetItem(query1.value(5).toString()));
-		ui.tableWidget->setItem(row, 6, new QTableWidgetItem(unitPrice));
-		ui.tableWidget->setItem(row, 7, new QTableWidgetItem(query1.value(6).toString()));
-
-		if (qty == "Not in stock")
-		{
-			QWidget* base = new QWidget(ui.tableWidget);
-			QPushButton* addToStockBtn = new QPushButton("Add To Stock", base);
-			addToStockBtn->setMaximumWidth(120);
-			addToStockBtn->setMinimumWidth(120);
-			QHBoxLayout *layout = new QHBoxLayout;
-			layout->setContentsMargins(0, 0, 0, 0);
-			layout->addWidget(addToStockBtn);
-			layout->insertStretch(1);
-			base->setLayout(layout);
-			ui.tableWidget->setCellWidget(row, 8, base);
-			base->show();
-		}
-		else
-		{
-			QWidget* base = new QWidget(ui.tableWidget);
-			QPushButton* updateBtn = new QPushButton("Update", base);
-			updateBtn->setMaximumWidth(100);
-			QPushButton* removeBtn = new QPushButton("Remove", base);
-			removeBtn->setMaximumWidth(100);
-
-			QHBoxLayout *layout = new QHBoxLayout;
-			layout->setContentsMargins(0, 0, 0, 0);
-
-			layout->addWidget(updateBtn);
-			layout->addWidget(removeBtn);
-			layout->insertStretch(2);
-			base->setLayout(layout);
-			ui.tableWidget->setCellWidget(row, 8, base);
-			base->show();
-		}
+	}
+	else
+	{
+		displayStockItems();
 	}
 }
 
@@ -218,17 +193,11 @@ void ESManageStockItems::displayStockItems()
 			QString itemCategoryId = query1.value(1).toString();
 			bool deleted = query1.value(7).toBool();
 
-// 			int row = ui.tableWidget->rowCount();
-// 			ui.tableWidget->insertRow(row);
-// 			ui.tableWidget->setItem(row, 0, new QTableWidgetItem(query1.value(2).toString()));
-// 			ui.tableWidget->setItem(row, 1, new QTableWidgetItem(query1.value(3).toString()));
-
 			rowItems.append(query1.value(2).toString());
 			rowItems.append(query1.value(3).toString());
 			QSqlQuery query3("SELECT * FROM item_category WHERE itemcategory_id=" + itemCategoryId);
 			if (query3.first())
 			{
-				//ui.tableWidget->setItem(row, 2, new QTableWidgetItem(query3.value(2).toString()));
 				rowItems.append(query3.value(2).toString());
 			}
 
@@ -249,58 +218,17 @@ void ESManageStockItems::displayStockItems()
 				}
 			}
 
-// 			ui.tableWidget->setItem(row, 3, new QTableWidgetItem(qty));
-// 			ui.tableWidget->setItem(row, 4, new QTableWidgetItem(minQty));
-// 			ui.tableWidget->setItem(row, 5, new QTableWidgetItem(query1.value(5).toString()));
-// 			ui.tableWidget->setItem(row, 6, new QTableWidgetItem(unitPrice));
-// 			ui.tableWidget->setItem(row, 7, new QTableWidgetItem(query1.value(6).toString()));
-
 			rowItems.append(qty);
 			rowItems.append(minQty);
 			rowItems.append(query1.value(5).toString());
 			rowItems.append(unitPrice);
 			rowItems.append(query1.value(6).toString());
-			bool inStock = false;
+			bool inStock = true;
 
 			if (qty == "Not in stock" && !deleted)
 			{
-				inStock = true;
-// 				QWidget* base = new QWidget(ui.tableWidget);
-// 				QPushButton* addToStockBtn = new QPushButton("Add To Stock", base);
-// 				addToStockBtn->setMaximumWidth(120);
-// 				addToStockBtn->setMinimumWidth(120);
-// 				QHBoxLayout *layout = new QHBoxLayout;
-// 				layout->setContentsMargins(0, 0, 0, 0);
-// 				layout->addWidget(addToStockBtn);
-// 				layout->insertStretch(1);
-// 				base->setLayout(layout);
-// 				ui.tableWidget->setCellWidget(row, 8, base);
-// 				base->show();
+				inStock = false;
 			}
-// 			else if (!deleted)
-// 			{
-// 				QWidget* base = new QWidget(ui.tableWidget);
-// 				QPushButton* updateBtn = new QPushButton("Update", base);
-// 				updateBtn->setMaximumWidth(100);
-// 				QPushButton* removeBtn = new QPushButton("Remove", base);
-// 				removeBtn->setMaximumWidth(100);
-// 				
-// 
-// 				connect(removeBtn, SIGNAL(clicked()), &m_buttonSignalMapper, SLOT(map()));
-// 				m_buttonSignalMapper.setMapping(removeBtn, itemId);
-// 				connect(&m_buttonSignalMapper, SIGNAL(mapped(QString)), this, SLOT(slotRemove(QString)));
-// 
-// 
-// 				QHBoxLayout *layout = new QHBoxLayout;
-// 				layout->setContentsMargins(0, 0, 0, 0);
-// 				layout->addWidget(updateBtn);
-// 				layout->addWidget(removeBtn);
-// 				layout->insertStretch(2);
-// 				base->setLayout(layout);
-// 				ui.tableWidget->setCellWidget(row, 8, base);
-// 				base->show();
-// 			}
-
 			displayStockTableRow(rowItems, itemId, inStock);
 		}
 	}
