a093960dd72689cb34435eb2b9964bfbcebc6659
shan <shanchathuranga@gmail.com>
2016-05-24 21:09:52 +0530

    reporting added

--- estorecpp/src/main.cpp
+++ estorecpp/src/main.cpp
@@ -3,6 +3,8 @@
 #include <QtWidgets/QApplication>
 #include <QDebug>
 #include "easylogging++.h"
+#include <KDReportsReport.h>
+#include <KDReportsTextElement.h>
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -14,6 +16,27 @@ int main(int argc, char *argv[])
 	w.showMaximized();
 	LOG(INFO) << "Initializing the system...";
 	ES::MainWindowHolder::instance()->setMainWindow(&w);
+
+	// Create a report
+	KDReports::Report report;
+
+	// Add a text element for the title
+	KDReports::TextElement titleElement(QObject::tr("Hello World!"));
+	titleElement.setPointSize(18);
+	report.addElement(titleElement, Qt::AlignHCenter);
+
+	// add 20 mm of vertical space:
+	report.addVerticalSpacing(20);
+
+	// add some more text
+	KDReports::TextElement textElement(QObject::tr("This is a report generated with KDReports"));
+	report.addElement(textElement, Qt::AlignLeft);
+
+	QPrinter printer;
+	printer.setOutputFormat(QPrinter::PdfFormat);
+	printer.setOutputFileName("../print.pdf");
+
+	report.print(&printer, 0);
 
 	return a.exec();
 }
