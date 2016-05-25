faccf57c4b042f0585ad21252ae1d6d2422326b3
imoremada <imoremada@gmail.com>
2016-05-25 16:21:26 +0530

    file open dialog added for backups

--- estorecpp/includes/esbackuprestore.h
+++ estorecpp/includes/esbackuprestore.h
@@ -14,6 +14,7 @@ public:
 
 	public slots:
 	void slotTest();
+	void slotOpenFileDialog();
 
 private:
 	Ui::BackupRestoreWidget ui;
--- estorecpp/src/esbackuprestore.cpp
+++ estorecpp/src/esbackuprestore.cpp
@@ -1,10 +1,12 @@
 #include "esbackuprestore.h"
-
+#include <QFileDialog> 
 
 ESBackupRestore::ESBackupRestore(QWidget *parent /*= 0*/)
 : QWidget(parent)
 {
 	ui.setupUi(this);
+
+	QObject::connect(ui.backupDirectoryButton, SIGNAL(clicked()), this, SLOT(slotOpenFileDialog()));
 }
 
 ESBackupRestore::~ESBackupRestore()
@@ -15,4 +17,10 @@ ESBackupRestore::~ESBackupRestore()
 void ESBackupRestore::slotTest()
 {
 
+}
+
+void ESBackupRestore::slotOpenFileDialog()
+{
+	QString backupDirectoryPath = QFileDialog::getOpenFileName(this, tr("Open Backup File"));
+	ui.backupDirectoryPath->setText(backupDirectoryPath);
 }
