0303f84b1057134fc7cfba2f32517140e29172a0
shan <shanchathuranga@gmail.com>
2016-07-01 19:06:55 +0530

    ui updated

--- estorecpp/src/essalesummaryreport.cpp
+++ estorecpp/src/essalesummaryreport.cpp
@@ -6,8 +6,15 @@ ESSalesSummary::ESSalesSummary(QWidget *parent /*= 0*/) : QWidget(parent)
 	ui.setupUi(this);
 
 	QStringList headerLabels;
-	headerLabels.append("Payment Mode");
-	headerLabels.append("Sub Total");
+	headerLabels.append("CASH");
+	headerLabels.append("CREDIT");
+	headerLabels.append("CHEQUE");
+	headerLabels.append("CARD");
+	headerLabels.append("LOYALTY");
+
+	QFont font = this->font();
+	font.setBold(true);
+	font.setPointSize(12);
 
 	ui.tableWidgetByUser->setHorizontalHeaderLabels(headerLabels);
 	ui.tableWidgetByUser->horizontalHeader()->setStretchLastSection(true);
@@ -15,6 +22,30 @@ ESSalesSummary::ESSalesSummary(QWidget *parent /*= 0*/) : QWidget(parent)
 	ui.tableWidgetByUser->setEditTriggers(QAbstractItemView::NoEditTriggers);
 	ui.tableWidgetByUser->setSelectionBehavior(QAbstractItemView::SelectRows);
 	ui.tableWidgetByUser->setSelectionMode(QAbstractItemView::SingleSelection);
+	ui.tableWidgetByUser->setFont(font);
+
+	ui.tableWidgetByUser->verticalHeader()->setFont(font);
+	ui.tableWidgetByUser->verticalHeader()->setMinimumWidth(200);
+
+	//////////////////////////////////////////////////////////////////////////
+
+	QStringList headerLabels2;
+	headerLabels2.append("CASH");
+	headerLabels2.append("CREDIT");
+	headerLabels2.append("CHEQUE");
+	headerLabels2.append("CARD");
+	headerLabels2.append("LOYALTY");
+
+	ui.tableWidgetTotal->setHorizontalHeaderLabels(headerLabels2);
+	ui.tableWidgetTotal->horizontalHeader()->setStretchLastSection(true);
+	ui.tableWidgetTotal->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+	ui.tableWidgetTotal->setEditTriggers(QAbstractItemView::NoEditTriggers);
+	ui.tableWidgetTotal->setSelectionBehavior(QAbstractItemView::SelectRows);
+	ui.tableWidgetTotal->setSelectionMode(QAbstractItemView::SingleSelection);
+	ui.tableWidgetTotal->setFont(font);
+
+	ui.tableWidgetTotal->verticalHeader()->setFont(font);
+	ui.tableWidgetTotal->verticalHeader()->setMinimumWidth(200);
 
 	QObject::connect(ui.generateButton, SIGNAL(clicked()), this, SLOT(slotGenerate()));
 
